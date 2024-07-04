5fea71e95921da96f48548b659a88346904e5531
Sam <samuel@encodevlabs.com>
2024-07-04 19:56:01 +0000

    moved BottomSheet to Stack navigaton

--- navigation/Stack/index.js
+++ navigation/Stack/index.js
@@ -1,20 +1,32 @@
-import * as React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import NotFound from "./screens/NotFound";
 import BottomTabNavigator from "../BottomTab";
+import BottomSheetComponent from "../../components/BottomSheet";
 
 const Stack = createStackNavigator();
 
 export default function StackNavigator() {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={BottomTabNavigator} />
-      <Stack.Screen
-        name="NotFound"
-        component={NotFound}
-        options={{ title: "Oops!" }}
-      />
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator screenOptions={{ headerShown: false }}>
+			<Stack.Screen name="Home" component={BottomTabNavigator} />
+			<Stack.Screen
+				name="Sheet"
+				component={BottomSheetComponent}
+				options={{
+					headerShown: false,
+					gestureEnabled: true,
+					cardOverlayEnabled: true,
+					presentation: "transparentModal",
+					animationTypeForReplace: "push",
+					animation: "fade",
+				}}
+			/>
+			<Stack.Screen
+				name="NotFound"
+				component={NotFound}
+				options={{ title: "Oops!" }}
+			/>
+		</Stack.Navigator>
+	);
 }
